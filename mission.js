5f3b10a70a2c395d9a9f072fe810f5e0fbb2935b
Johnathaniel16 <vel21017@byui.edu>
2025-09-27 22:36:00 -0600

    fixed typo

--- mission.js
+++ mission.js
@@ -10,7 +10,7 @@ function changeTheme() {
   if (themeSelector.value === 'dark') {
     // Step 4a: If it is "dark", add the dark class to body and change the logo
     document.body.classList.add('dark');
-    logo.src = 'byui-logo_white.webp'; // use the white logo in dark mode
+    logo.src = 'byui-logo_white.png'; // use the white logo in dark mode
   } else {
     // Step 4b: Otherwise, remove the dark class and set the blue logo
     document.body.classList.remove('dark');
